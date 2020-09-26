7b2b4a2d9853c843ea604f2fcceadf3959f64d94
Yusuf619Hendricks <yusuf69hendricks@gmail.com>
2020-09-26 18:21:17 +0200

    added structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// NAVABAR
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+// NAVBAR 
