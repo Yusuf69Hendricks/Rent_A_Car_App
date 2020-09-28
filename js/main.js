4422d20ee0deca6673660d6a193fc8421b4d14fe
Yusuf619Hendricks <yusuf69hendricks@gmail.com>
2020-09-28 14:07:37 +0200

    added complete home page structure and added created other pages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // NAVABAR
 function myFunction() {
-    var x = document.getElementById("mynavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 // NAVBAR 
